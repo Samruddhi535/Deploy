59cfbfb43b98cfa177523487d94ded29fc1fea7f
Samruddhi Renakale <samruddhirenakale3@gmail.com>
2025-01-27 17:46:38 +0530

    changed cors

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,7 @@ import router from "./routes/userRoutes.js";
 import errorHandler from "./middleware/errorHandler.js";
 
 const app = express();
-app.use(
-  cors({
-    origin: "http://localhost:3000", // Allow only the frontend URL
-    credentials: true, // Allow cookies if used
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 app.use(express.json());
